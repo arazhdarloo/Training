bf5d4784f248780d0c527aab41bc784227aad594
Alireza <ar.azhdarloo@gmail.com>
2025-12-01 00:21:03 +0330

    C# - Calculator (UNI)

---
+++ C#/Calculator/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter first number: ");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.Write("Enter operator (+, -, /, *): ");
+                char operate = char.Parse(Console.ReadLine());
+                Console.Write("Enter secound number: ");
+                int secoundNumber = int.Parse(Console.ReadLine());
+
+                switch (operate)
+                {
+                    case '*':
+                        Console.WriteLine("result = " + (firstNumber * secoundNumber));
+                        break;
+                    case '/':
+                        Console.WriteLine("result = " + (firstNumber / secoundNumber));
+                        break;
+                    case '+':
+                        Console.WriteLine("result = " + (firstNumber + secoundNumber));
+                        break;
+                    case '-':
+                        Console.WriteLine("result = " + (firstNumber - secoundNumber));
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            }
+        }
+    }
+}
