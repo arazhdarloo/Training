a09dacbbf6375e4d76c093d58451551f4b9310d3
Alireza <ar.azhdarloo@gmail.com>
2025-12-01 00:33:42 +0330

    C# - Perimeter and Area of ​​a Rectangle (UNI)

---
+++ C#/Perimeter and Area of ​​a Rectangle/Perimeter and Area of ​​a Rectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimeter_and_Area_of_​​a_Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter width: ");
+                int width = int.Parse(Console.ReadLine());
+                Console.Write("Enter height: ");
+                int height = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Area is " + (width * height));
+                Console.WriteLine("Perimeter is " + 2 * (width + height));
+            }
+        }
+    }
+}
