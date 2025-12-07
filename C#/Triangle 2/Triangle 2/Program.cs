d072c5471064998b472ac00d34a444f8aedc23ab
Alireza <ar.azhdarloo@gmail.com>
2025-12-07 23:52:59 +0330

    C# - Triangle 2 (UNI)

---
+++ C#/Triangle 2/Triangle 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sides");
+            Console.Write("enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("enter c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+                Console.WriteLine("You can make a triangle.");
+            else Console.WriteLine("You can't make a triangle.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
