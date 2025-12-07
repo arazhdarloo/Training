db5a121295f870170ccba24f1420fc8fbc58a6fa
Alireza <ar.azhdarloo@gmail.com>
2025-12-08 00:04:03 +0330

    C# - Season Finder 2 (UNI)

---
+++ C#/Season Finder 2/Season Finder 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Season_Finder_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter season number: ");
+            byte seasonNumber = byte.Parse(Console.ReadLine());
+
+            switch (seasonNumber)
+            {
+                case 1:
+                    Console.WriteLine("spring");
+                    break;
+                case 2:
+                    Console.WriteLine("summer");
+                    break;
+                case 3:
+                    Console.WriteLine("fall");
+                    break;
+                case 4:
+                    Console.WriteLine("winter");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Season Finder 2/Season Finder 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Season Finder 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Season Finder 2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc227dd1-7d3a-4813-a543-9f474e8e02eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
