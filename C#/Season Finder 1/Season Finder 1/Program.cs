52b08e3157ac86a7f44f7c1782ff865ea15a8897
Alireza <ar.azhdarloo@gmail.com>
2025-12-08 00:00:29 +0330

    C# - Season Finder 1 (UNI)

---
+++ C#/Season Finder 1/Season Finder 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Season_Finder_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter season number: ");
+            byte seasonNumber = byte.Parse(Console.ReadLine());
+
+            if (seasonNumber == 1)
+                Console.WriteLine("spring");
+            else if (seasonNumber == 2)
+                Console.WriteLine("summer");
+            else if (seasonNumber == 3)
+                Console.WriteLine("fall");
+            else if (seasonNumber == 4)
+                Console.WriteLine("winter");
+            else Console.WriteLine("error");
+
+            Console.ReadKey();
+        }
+    }
+}
