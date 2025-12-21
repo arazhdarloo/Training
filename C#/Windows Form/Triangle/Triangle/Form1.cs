bdc737e237e8eb05e6ed9895edce4049ce8fa1ee
Alireza <ar.azhdarloo@gmail.com>
2025-12-22 01:47:24 +0330

    C# - Triangle (WF)(UNI)

---
+++ C#/Windows Form/Triangle/Triangle/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = int.Parse(textBox3.Text);
+
+            if(a + b > c &&  b + c > a && a + c > b) {
+                label1.Text = "YES";
+            }
+            else
+            {
+                label1.Text = "NO";
+            }
+        }
+    }
+}
