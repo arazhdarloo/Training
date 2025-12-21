677a59c565a13641bf3b5f4ce271bc5e8e8f1864
Alireza <ar.azhdarloo@gmail.com>
2025-12-21 23:32:26 +0330

    C# - Perimeter and Area of ​​a Rectangle (WF)(UNI)

---
+++ C#/Windows Form/Perimeter and Area of ​​a Rectangle/Perimeter and Area of ​​a Rectangle/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perimeter_and_Area_of_​​a_Rectangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "the perimeter is " + (2 * (float.Parse(textBox1.Text) + float.Parse(textBox2.Text)));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "the area is " + (float.Parse(textBox1.Text) * float.Parse(textBox2.Text));
+
+        }
+    }
+}
---
+++ C#/Windows Form/Perimeter and Area of ​​a Rectangle/Perimeter and Area of ​​a Rectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perimeter_and_Area_of_​​a_Rectangle
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
