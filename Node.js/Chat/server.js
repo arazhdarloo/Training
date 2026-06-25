8222e15f1f9c002c65bf8cc03a512450fd7cdff1
Alireza <ar.azhdarloo@gmail.com>
2026-06-25 15:11:32 +0330

    Node.js - Chat: initial socket and use broadcast

--- Node.js/Chat/server.js
+++ Node.js/Chat/server.js
@@ -12,8 +12,12 @@ const io = socket(server)
 app.use(express.static(path.join(__dirname, 'public')))
 
 io.on('connection', (socket) => {
-    console.log('someone connected!');
-});
+    console.log('someone connected!')
+
+    socket.emit('message', "Welcome to my chat!")
+
+    socket.broadcast.emit('message', "A user joined to chat")
+})
 
 server.listen(PORT, () => {
     console.log('listening on port ' + PORT)
